85c0ae9d45560bbcea99e2d29409ab3c7e42f839
JuanMateus <105870925+Jmateus1014@users.noreply.github.com>
2022-06-28 18:58:49 -0500

    Cambiando base URl por git

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/biblioteca/public/';
+    public $baseURL = 'https://jmateus1014.github.io/biblioteca/public/';
 
     /**
      * --------------------------------------------------------------------------
