de458598d3843f926b144cc8440faa9c570ee4b9
Juan Mateus <mateusjuan454@gmail.com>
2022-06-28 18:56:08 -0500

    Primer commit

---
+++ app/Views/libros/Editar.php
@@ -0,0 +1,27 @@
+<?= $cabecera ?>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Ingresar datos del libro</h5>
+            <p class="card-text">
+                <form method="post" action="<?= site_url('/actualizar'); ?>" enctype="multipart/form-data">
+                    <input type="hidden" value="<?= $libro['id']; ?>" name="id">
+                    <div class="form-group">
+                        <label for="nombre">Nombre:</label>
+                        <input id="nombre" class="form-control" type="text" name="nombre" value="<?= $libro['nombre'];?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="my-input">Imagen actual:</label><br>
+                        <img class="img-thumbnail" src="<?= base_url(); ?>/uploads/<?= $libro['imagen']; ?>" width="100">
+                    </div>
+                    <div class="form-group">
+                        <label for="imagen">Imagen:</label>
+                        <input id="imagen" class="form-control-file" type="file" name="imagen">
+                    </div>
+                    <button class="btn btn-success" type="submit">Actualizar</button>
+                    <a href="<?= base_url(); ?>" class="btn btn-danger" type="button">Cancelar</a>
+                </form>
+            </p>
+        </div>
+    </div>
+
+<?= $pie ?>
