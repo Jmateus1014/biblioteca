4d7a556fd501502e239bf212e7490a556af49efb
JuanMateus <105870925+Jmateus1014@users.noreply.github.com>
2022-06-28 19:03:19 -0500

    Volviendo a la base URL localhost

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://jmateus1014.github.io/biblioteca/public/';
+    public $baseURL = 'http://localhost/biblioteca/public/';
 
     /**
      * --------------------------------------------------------------------------
